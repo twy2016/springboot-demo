33830cb4ee55b5bc82ca4dcb47b64d9afa59f8b8
gongpeng <whgp1994@163.com>
2020-10-15 17:35:23 +0800

    代码提交

---
+++ sessionshare02/src/main/java/com/twy/sessionshare02/Sessionshare02Application.java
@@ -0,0 +1,13 @@
+package com.twy.sessionshare02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sessionshare02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sessionshare02Application.class, args);
+    }
+
+}
---
+++ sessionshare02/src/test/java/com/twy/sessionshare02/Sessionshare02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twy.sessionshare02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sessionshare02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
